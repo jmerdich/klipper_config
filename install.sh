949754b63022f6f2c56a08b3f2c7f09ebb705656
Jake Merdich <jake@merdich.com>
2023-04-07 19:31:15 +0100

    Allow moving printer.cfg in install script

--- install.sh
+++ install.sh
@@ -2,4 +2,11 @@
 # Copyright Jake Merdich 2022
 # SPDX-License-Identifier: 0BSD
 
-echo "[include $(dirname $(realpath $0))/printer.cfg]" > ~/printer.cfg
+if [ -z "$1" ]
+then
+DEST_FILE=~/printer.cfg
+else
+DEST_FILE="$1"
+fi
+
+echo "[include $(dirname $(realpath $0))/printer.cfg]" > $DEST_FILE
